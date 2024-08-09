29d6841619b693229de423b457fb5955ced37765
Lakshya <lakshyamittal23503@gmail.com>
2024-08-09 18:07:55 +0530

    made cheanges in 01_variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,9 +1,9 @@
 const accountId = 144553
 let accountEmail = "lakshyamittal23503"
 var accountPassword = "12345"
-accountCity = "Jaipur"
+accountCity = "Hyderabad"
 
-let accontState;
+let accontState;oiuhpoh
 /*
 prefer not to use "var"
 because of issue in block scope and functional scope
