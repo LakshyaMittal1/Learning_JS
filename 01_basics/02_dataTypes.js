9d408c54476d6e41ecdb7468783d02ad7583ce61
Lakshya Mittal <84320968+LakshyaMittal1@users.noreply.github.com>
2024-08-09 11:42:51 +0000

    Data Types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello") // we are using nodejs, not browser
+let state; // undefined
+console.log(state)
+
+// number => 2 to power 53
+
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
+
+// null is an object!!
