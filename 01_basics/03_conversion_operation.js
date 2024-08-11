52b1efba55c87c5829c426371afce6df8b6387f3
Lakshya <lakshyamittal23503@gmail.com>
2024-08-11 18:50:56 +0530

    conversion operation of data types

---
+++ 01_basics/03_conversion_operation.js
@@ -0,0 +1,33 @@
+let score = "true"
+console.log(typeof score);
+console.log(typeof(score))
+
+let valueNumber = Number(score)
+console.log(typeof valueNumber);
+console.log(valueNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 0
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
+
+
+
+
+
+
+// let value = 3
+// let negValue = -value
+
+// // console.log(negValue);
+
+// let str1 = "hello "
+// let str2 = "Lakshya"
+
+// console.log(str1 + str2);
+
