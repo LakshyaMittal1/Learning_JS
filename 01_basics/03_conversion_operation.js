ede13c7459069b54c6975c862d9831fc624af1bd
Lakshya <lakshyamittal23503@gmail.com>
2024-08-11 21:35:16 +0530

    conversion operation (updated)

--- 01_basics/03_conversion_operation.js
+++ 01_basics/03_conversion_operation.js
@@ -1,25 +1,25 @@
-let score = "true"
-console.log(typeof score);
-console.log(typeof(score))
+// let score = "true"
+// console.log(typeof score);
+// console.log(typeof(score))
 
-let valueNumber = Number(score)
-console.log(typeof valueNumber);
-console.log(valueNumber)
+// let valueNumber = Number(score)
+// console.log(typeof valueNumber);
+// console.log(valueNumber)
 
-// "33" => 33
-// "33abc" => NaN
-// true => 1; false => 0
+// // "33" => 33
+// // "33abc" => NaN
+// // true => 1; false => 0
 
-let isLoggedIn = "lakshya"
+// let isLoggedIn = "lakshya"
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
 
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
-let num = 33
-let stringNum = String(num)
-console.log(stringNum);
-console.log(typeof stringNum);
+// let num = 33
+// let stringNum = String(num)
+// console.log(stringNum);
+// console.log(typeof stringNum);
 
 
 // --------------------------------------------------------
@@ -29,10 +29,29 @@ console.log(typeof stringNum);
 // let value = 3
 // let negValue = -value
 
-// // console.log(negValue);
+// console.log(negValue);
 
 // let str1 = "hello "
 // let str2 = "Lakshya"
 
 // console.log(str1 + str2);
 
+// console.log("1" + 2);
+// console.log(1 + "2")
+// console.log("1" + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+// console.log(1 + "3" + 2 + 2)
+
+// console.log(+true) // 1
+// console.log(+"") // 0
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+
+console.log(num1, num2, num3)
+
+let gameCounter = 100
+gameCounter++
+++gameCounter
+console.log(gameCounter)
